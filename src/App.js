d87d4bbf72f4c3f0ade7416dacc446fb6976c9fe
Brynner Ventura <bventura03@hotmail.com>
2022-05-10 15:42:29 -0400

    I believe auth is setup in the frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { CheckSession } from './services/auth'
+import Home from './pages/Home'
+
 
 function App() {
+
+  const [authenticated, toggleAuthenticated] = useState(false)
+  const [user, setUser] = useState({
+    userName: '',
+    id: NaN
+  })
+
+  const checkToken = async () => {
+    const user = await CheckSession()
+    setUser(user)
+    toggleAuthenticated(true)
+  }
+
+
+  const handleLogOut = () =>{
+    setUser(null)
+  }
+
+  useEffect(()=>{
+    const token = localStorage.getItem('token')
+    if (token) {
+      checkToken()
+    }
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* <Nav authenticated={authenticated} user={user} handleLogOut={handleLogOut} /> */}
+      <main>
+        <Routes>
+          {/* <Route path='/' exact element={<Home/>} /> */}
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
