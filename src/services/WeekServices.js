5b5a020f022642b51968f647b2ea0191526ad1fd
Brynner Ventura <bventura03@hotmail.com>
2022-05-11 09:29:41 -0400

    progress

---
+++ src/services/WeekServices.js
@@ -0,0 +1,43 @@
+import Client from './api'
+
+export const GetWeeks = async () => {
+    try {
+        const res = await Client.get('/week')
+        return res.data
+    } catch (error) {
+        throw error
+    }
+}
+
+export const EditWeek = async (data) => {
+    try {
+        const res = await Client.put(`/week/${data.id}`, data)
+        console.log(res.data)
+        return res.data
+    } catch (error) {
+        throw error
+    }
+}
+
+export const DeleteWeek = async (data) => {
+    try {
+        const res = await Client.delete(`/week/${data.id}`)
+        console.log(res.data)
+        return res.data
+    } catch (error) {
+        throw error
+    }
+}
+
+export const UsersWeek = async (data) => {
+    try {
+        if(isNaN(data)) {
+            data = 1
+        }
+
+        const res = await Client.get(`/week/user/${data}`)
+        return res.data
+    } catch (error) {
+        throw error
+    }
+}
