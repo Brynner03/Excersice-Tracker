6afd7885ca8effb3711e4326323cc659a99f2664
Brynner Ventura <bventura03@hotmail.com>
2022-05-11 10:48:26 -0400

    Making Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,85 @@
+import React, {useState, useEffect} from "react";
+import { Link } from 'react-router-dom'
+
+
+const Navbar = ({ authenticated, user, handleLogOut }) => {
+    const [toggleMenu, setToggleMenu] = useState(false)
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+  
+  
+    const toggleNav = () => {
+      setToggleMenu(!toggleMenu)
+    }
+  
+    useEffect(() => {
+      const changeWidth = () => {
+        setScreenWidth(window.innerWidth)
+      }
+      window.addEventListener('resize', changeWidth)
+  
+      // cleanup function //
+      return () => {
+        window.removeEventListener('resize', changeWidth)
+      }
+    }, [])
+
+    
+    let authenticatedOptions
+    if (user) {
+        authenticatedOptions = (
+            <div className = 'header'>
+                <nav className='navigation'>
+                    {(toggleMenu || screenWidth > 900) && (
+                        <ul className='list'>
+                            <li className="navLinks">Welcome {user.userName}</li>
+                            <li className='navLinks'>
+                                <Link to='/'>Home</Link>
+                            </li>
+                            <li className = "navLinks">
+                                <Link className='navLinks' to={'/workout'}>
+                                    Workouts
+                                </Link>
+                            </li>
+                            <li className="navLinks">
+                             <Link className="navLinks" onClick={handleLogOut} to="/">
+                                     Sign Out
+                                </Link>
+                </li>
+                        </ul>
+                    )}
+                    <div classNAme="navLinks">
+                        <button onClick={toggleNav} className="navBtn">
+
+                        </button>
+                    </div>
+                </nav>
+            </div>
+        )
+    }
+    let publicOptions = (
+        <header>
+            <div className="header">
+                <nav className='navigation'>
+                    <ul className='list'>
+                      <li className='navLinks'>
+                          <Link to='/'> Home </Link>
+                      </li>
+                        <li className='navLinks'>
+                            <Link to='/login'>Login</Link>
+                        </li>
+                        <li className="navLinks">
+                            <Link to='/register'>Register</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+    return (
+        <header>
+            {authenticated && user ? authenticatedOptions : publicOptions}
+        </header>
+    )
+}
+
+export default Navbar
