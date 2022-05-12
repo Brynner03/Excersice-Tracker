4f0b606e31f9deb34be1429fd227cd4c4861c9e4
Brynner Ventura <bventura03@hotmail.com>
2022-05-12 16:46:27 -0400

    delete button works

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -10,16 +10,11 @@ const Workout = ({user, authenticated}) => {
     let navigate = useNavigate()
     
     const [workouts, setWorkouts] = useState([])
-    const [editWorkout, editWorkouts] = useState([false, 1])
     const [editReps, setReps] = useState('')
     const [editSets, setSets] = useState('')
     const [editWeights, setWeights] = useState('')
     const [editName, setName] = useState('')
     
-    const navDelete = (workout) => {
-        navigate(`../workout/${workout.id}`)
-        delWorkout(workout)
-    }
     const delWorkout = async (workout) => {
         const sendload = {
             ...workout,
@@ -29,31 +24,6 @@ const Workout = ({user, authenticated}) => {
         console.log(payload)
         console.log('Deleted workout')
     }
-    const updateWorkout = (workout) => {
-        editWorkouts([!editWorkout[0], workout.id])
-        setName(workout.name)
-        setSets(workout.sets)
-        setReps(workout.reps)
-        setWeights(workout.weight)
-    }
-    const handleChange = (e) => {
-        setName(e.target.value)
-        setSets(e.target.value)
-        setReps(e.target.value)
-        setWeights(e.target.value)
-    }
-    const sendit = async (workout) => {
-        const sendload = {
-            ...workout,
-            name: editName,
-            sets: editSets,
-            reps: editReps,
-            weight: editWeights
-        };
-        console.log(sendload);
-        const payload = await EditWorkout(sendload);
-        console.log(payload);
-    };
     
     useEffect(() => {
         const handleWorkout = async () => {
@@ -62,14 +32,13 @@ const Workout = ({user, authenticated}) => {
         }
         handleWorkout()
     },[] )
-    
-
 
     return user && authenticated && workouts ? (
         <div>
             <div className='container'>
                 <div className="profile">
                     <div className='userWorkouts'>
+                        <button>Track new workout</button>
                         {workouts.slice('').reverse().map((workout) => (
                             
                             <div className='workouts' key={workout.id}>
@@ -77,6 +46,7 @@ const Workout = ({user, authenticated}) => {
                                 <div className="reps"> Reps: {workout.reps}</div>
                                 <div className='sets'> Sets:{workout.sets} </div>
                                 <div className="weight"> Weight: {workout.weight}</div>
+                                <button onClick={() => delWorkout(workout)} >Delete workout</button>
                                     </div>
                         ))}
                     </div>
